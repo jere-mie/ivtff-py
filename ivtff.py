72f7b8397c0e91a2cbaced895a82e3c80fa41940
Jeremie Bornais <jeremiejbornais@gmail.com>
2023-02-25 06:51:00 +0000

    add gitignore, comments, and regex

---
+++ ivtff.py
@@ -0,0 +1,5 @@
+# defining some useful regular expressions
+RE_LOCUS_ID = r'<(f\d+[vr])\.\d+,[@+*\-=&~]\w+(;T)?>'
+RE_INLINE_COMMENT = r'<.*?>'
+RE_HIGH_ASCII = r'@(\d{3});'
+RE_UNCERTAIN_READING = r'\[(.*?)\]'
